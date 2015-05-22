665a688e6238f3c73b21cad7d64f6145a978beca
yuvalsegall <y.segall@gmail.com>
2015-05-22 20:18:23 +0300

    with new controller

--- src/view/GameSceneController.java
+++ src/view/GameSceneController.java
@@ -29,6 +29,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.FileChooser;
@@ -350,6 +351,8 @@ public class GameSceneController implements Initializable {
     private static final int[] COMP_BET_NUMBERS = null;
 
     private boolean isErrorMessageShown;
+    @FXML
+    private ImageView wheelImageView;
 
     /**
      * Initializes the controller class.
