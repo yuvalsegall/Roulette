12b65d79dabfbba5de580f8c76bc2be2a386466d
yuvalsegall <y.segall@gmail.com>
2015-05-22 17:38:53 +0300

    yo!

--- src/view/GameSceneController.java
+++ src/view/GameSceneController.java
@@ -2,14 +2,6 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
- asdcascascsa
->>>>>>> origin/master
-=======
- asdcascascsa
->>>>>>> origin/master
  */
 package view;
 
